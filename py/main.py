9554f0f9a02670233f02b5f2ca6b5a55dbe5b98b
Shlomi Matichin <shlomomatichin@gmail.com>
2018-07-18 10:07:22 +0300

    Changed module name conflict with python3

--- py/main.py
+++ py/main.py
@@ -2,10 +2,10 @@ import os
 import sys
 sys.path.append(os.path.join(os.environ['HOME'], ".vim", "bundle", "vimpygoodies", "py"))
 import vim
-import entrypoints
+import goodiesentrypoints
 
 line1 = int(argv.pop(0)) - 1
 line2 = int(argv.pop(0)) - 1
 command = argv.pop(0)
-entrypoints.reloadWhatChanged()
-entrypoints.invoke(command, [line1, line2] + argv)
+goodiesentrypoints.reloadWhatChanged()
+goodiesentrypoints.invoke(command, [line1, line2] + argv)
